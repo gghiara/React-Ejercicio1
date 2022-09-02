d286676bc77dad3af51a672bc75e67e553ffa8b3
Gaston Ghiara <tongarock@gmail.com>
2022-09-02 15:27:03 -0300

    Clase 3

--- js/app.js
+++ js/app.js
@@ -2,4 +2,24 @@
 let myVar = 0;
 var myVar2 = 1;
 
-const myDom = document;
+const myDoc = document;
+
+console.log(myDoc);
+
+let myH1 = myDoc.getElementsByTagName("h1");
+
+let myH3 = myDoc.getElementById("mySuperH3");
+
+console.log(myH1);
+console.log(myH3);
+
+myH3.innerText = "Estoy modificando el texto dentro del H3"
+
+// const myObj = {
+//     "atributo" : "valor"
+// }
+
+// const myObjeto2 = {
+//     atributo: "valor",
+//     atributo2: "otro valor"
+// }
